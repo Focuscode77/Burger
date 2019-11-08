3d88abf09ccfd326e96721190ca79ab6d0735647
Joshua Coleman <procoleman77@outlook.com>
2019-11-08 17:14:42 -0500

    Created the logic for the database

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,14 @@
-## Schema
+
 
 CREATE DATABASE burger_db;
 USE burger_db;
 
 CREATE TABLE resturant
 (
-	-- id int NOT NULL AUTO_INCREMENT,
-	-- name varchar(255) NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
+	burger varchar(255) NOT NULL,
 	-- sleepy BOOLEAN DEFAULT false,
-	-- PRIMARY KEY (id)
+	 PRIMARY KEY (id)
 );
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,13 @@
 -- All inserts go here floowing after the sql file is finished. Remember to copy and paste this into mysql work
 -- bench in order for it to work!
+
+---------------------------------List of Burgers to chosee from in my database----------------------
+
+INSERT INTO resturant (burger) VALUES ('Chesseburger');
+
+INSERT INTO resturant (burger) VALUES ('Bacon chesseburger');
+
+INSERT INTO resturant (burger) VALUES ('Chicken chesseburger');
+
+INSERT INTO resturant (burger) VALUES ('Philadelphia chesseburger');
+
