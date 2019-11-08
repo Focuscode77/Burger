7d811f228409b8298003407db6a25fe276fd02ea
Joshua Coleman <procoleman77@outlook.com>
2019-11-08 17:53:58 -0500

    Added logic for orm and handlebars as well as model

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -4,13 +4,51 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
 
 
 //  NOTE! When doing the file for the index page do a res.render for handle bars
 
 
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burger: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burger", function(req, res) {
+    burger.create(["burger", "sleepy"], [req.body.burger, req.body.sleepy], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burger/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    cat.update(
+      {
+        sleepy: req.body.sleepy
+      },
+      condition,
+      function(result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      }
+    );
+  });
 
 
 
@@ -19,6 +57,8 @@ var cat = require("../models/burger.js");
 // Path Logic Goes here!!!
 
 
+// Export routes for server.js to use.
+module.exports = router;
 
 
 
